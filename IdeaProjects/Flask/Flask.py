f450c415b929d782087e5e5b726172556a0c67ca
nobelbill <nobelbill@gmail.com>
2015-04-07 11:49:22 +0900

    Initial commit

---
+++ IdeaProjects/Flask/Flask.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+import psutil
+
+app = Flask(__name__)
+
+
+@app.route('/cpu')
+def cpu():
+    return str(psutil.cpu_percent(interval=1))
+
+@app.route('/memory')
+def memory():
+    ret = "phy usage :\t" + str(psutil.phymem_usage().percent) + "%\nvir usage :\t" +str(psutil.virtmem_usage().percent)+ "%"
+    return ret;
+
+@app.route('/config/<config_user>')
+def config(config_user):
+    return 'config user =  %s' % config_user
+
+@app.route('/view/<user_id>')
+def view(user_id=None):
+    return render_template('user.html',user_id=user_id)
+
+def boot_make():
+    app=Flask(__name__)
+    Bootstrap(app)
+    return app
+
+if __name__ == '__main__':
+    print cpu()
+    print memory()
+
+    app.run(host='127.0.0.1')
